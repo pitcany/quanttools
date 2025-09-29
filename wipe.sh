4a873abe647341de5ef5a6796d865c608cb20508
pitcany <pitcany@gmail.com>
2025-09-28 21:53:05 -0700

    fixed wipe.sh

--- wipe.sh
+++ wipe.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-rm -rf .venv poetry.lock && poetry env list --full-path | awk '{print $1}' | xargs -r poetry env remove && poetry cache clear PyPI --all
+rm -rf .venv poetry.lock && \
+poetry env list --full-path --quiet | xargs -r -n1 poetry env remove && \
+poetry cache clear PyPI --all
